f1a674fae481fbaa2fe29d1e6945a037ad98cf99
Frank Boucher <fboucher@outlook.com>
2023-11-21 07:34:58 -0500

    fix url encoding issue

--- Url2Html.cs
+++ Url2Html.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Mime;
+using System.Web;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
@@ -20,28 +21,34 @@ namespace c5m.Functions
         [Function("Url2Html")]
         public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", "get")] HttpRequest req)
         {
-            string url = string.Empty;
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            try{
+                string url = string.Empty;
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
 
-            if (!string.IsNullOrEmpty(requestBody)) {
-                url = requestBody.Split('=').Last<string>();
+                if (!string.IsNullOrEmpty(requestBody)) {
+                    url = requestBody.Split('=').Last<string>();
+                }
+
+                if(string.IsNullOrEmpty(url))
+                {
+                    url = req.Query["url"];
+                }
+
+                var urlDecoded = HttpUtility.UrlDecode(url);
+                var urlParts = urlDecoded.Split('/');
+                string episode = urlParts.Last<string>();
+                string showName = urlParts[urlParts.Length -2];
+
+                var iframeCode = $"<iframe width='560' height='315' src='https://learn-video.azurefd.net/vod/player?show={showName}&ep={episode}&embedUrl=contoso.net/builders/cool-ai-resources/' title='Learn video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowfullscreen></iframe>";
+                
+                var htmlResponse = $"<html><body>Embed Video:<br/><textarea rows='10' cols='80'>{iframeCode}</textarea><br/><br/><button onclick='window.close()'>Close</button></body><html>";
+                var content = new ContentResult { Content = $"{htmlResponse}", ContentType = "text/html" };
+                return content;
             }
-
-            if(string.IsNullOrEmpty(url))
-            {
-                url = req.Query["url"];
+            catch(Exception ex){
+                return new ContentResult { Content = $"Oops! Error: {ex.Message}", ContentType = "text/html" };
             }
-
-            var urlParts = url.Split('/');
-            string episode = urlParts.Last<string>();
-            string showName = urlParts[urlParts.Length -2];
-
-            var iframeCode = $"<iframe width='560' height='315' src='https://learn-video.azurefd.net/vod/player?show={showName}&ep={episode}&embedUrl=contoso.net/builders/cool-ai-resources/' title='Learn video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowfullscreen></iframe>";
             
-            var htmlResponse = $"<html><body>Embed Video:<br/><textarea rows='10' cols='80'>{iframeCode}</textarea><br/><br/><button onclick='window.close()'>Close</button></body><html>";
-            var content = new ContentResult { Content = $"{htmlResponse}", ContentType = "text/html" };
-
-            return content;
         }
     }
 }
